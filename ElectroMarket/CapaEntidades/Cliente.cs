911a404145583b767b5b92e024b96cb8fc667ed7
Julian <cruzjulian.dev@gmail.com>
2023-10-02 01:29:37 -0300

    agrega todas las entidades

---
+++ ElectroMarket/CapaEntidades/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Dni { get; set; }
+        public DateTime FechaNacimiento  { get; set; }
+        public string Telefono { get; set; }
+        public string Domicilio { get; set; }
+        public bool Estado { get; set; }
+        public string FechaRegistro { get; set; }
+    }
+
+}
