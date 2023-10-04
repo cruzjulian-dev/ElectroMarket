ccbe47f398278a03717639b30087c0ef5d5729e7
Fabricio Blanco <fabriblanco.03@hotmail.com>
2023-10-04 11:58:15 -0300

    modifico titulo backup

--- ElectroMarket/CapaPresentacion/FormSuperAdmin/BackupBD.Designer.cs
+++ ElectroMarket/CapaPresentacion/FormSuperAdmin/BackupBD.Designer.cs
@@ -56,11 +56,11 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.SystemColors.Control;
             this.label5.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(12, 37);
+            this.label5.Location = new System.Drawing.Point(22, 40);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(312, 25);
+            this.label5.Size = new System.Drawing.Size(285, 25);
             this.label5.TabIndex = 16;
-            this.label5.Text = "Restaurar Bases de Datos";
+            this.label5.Text = "BackUp Bases de Datos";
             // 
             // label4
             // 
