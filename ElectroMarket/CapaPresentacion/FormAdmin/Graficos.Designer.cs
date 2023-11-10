691a2c260da29c8d96da7c7566921d9ade5d6a84
Fabricio Blanco <fabriblanco.03@hotmail.com>
2023-11-09 22:21:09 -0300

    agrego radio button y grafico

--- ElectroMarket/CapaPresentacion/FormAdmin/Graficos.Designer.cs
+++ ElectroMarket/CapaPresentacion/FormAdmin/Graficos.Designer.cs
@@ -28,32 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chartProdPorCat = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chartProdTop5 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.lblProductoMasVendido = new System.Windows.Forms.Label();
+            this.radioButtonClientes = new System.Windows.Forms.RadioButton();
+            this.radioButtonCantProd = new System.Windows.Forms.RadioButton();
+            this.lblCliente1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chartProdPorCat)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartProdTop5)).BeginInit();
             this.SuspendLayout();
             // 
             // chartProdPorCat
             // 
-            chartArea3.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
-            chartArea3.Name = "ChartArea1";
-            this.chartProdPorCat.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chartProdPorCat.Legends.Add(legend3);
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.Name = "ChartArea1";
+            this.chartProdPorCat.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartProdPorCat.Legends.Add(legend1);
             this.chartProdPorCat.Location = new System.Drawing.Point(12, 129);
             this.chartProdPorCat.Name = "chartProdPorCat";
-            series3.ChartArea = "ChartArea1";
-            series3.Legend = "Legend1";
-            series3.Name = "Series1";
-            this.chartProdPorCat.Series.Add(series3);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartProdPorCat.Series.Add(series1);
             this.chartProdPorCat.Size = new System.Drawing.Size(530, 390);
             this.chartProdPorCat.TabIndex = 0;
             this.chartProdPorCat.Text = "chart1";
@@ -61,19 +65,19 @@
             // 
             // chartProdTop5
             // 
-            chartArea4.Name = "ChartArea1";
-            this.chartProdTop5.ChartAreas.Add(chartArea4);
-            legend4.Name = "Legend1";
-            this.chartProdTop5.Legends.Add(legend4);
+            chartArea2.Name = "ChartArea1";
+            this.chartProdTop5.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartProdTop5.Legends.Add(legend2);
             this.chartProdTop5.Location = new System.Drawing.Point(608, 129);
             this.chartProdTop5.Name = "chartProdTop5";
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
-            series4.IsValueShownAsLabel = true;
-            series4.LabelForeColor = System.Drawing.Color.White;
-            series4.Legend = "Legend1";
-            series4.Name = "Series1";
-            this.chartProdTop5.Series.Add(series4);
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+            series2.IsValueShownAsLabel = true;
+            series2.LabelForeColor = System.Drawing.Color.White;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chartProdTop5.Series.Add(series2);
             this.chartProdTop5.Size = new System.Drawing.Size(530, 390);
             this.chartProdTop5.TabIndex = 1;
             this.chartProdTop5.Text = "chart1";
@@ -84,16 +88,52 @@
             this.lblProductoMasVendido.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblProductoMasVendido.Location = new System.Drawing.Point(610, 524);
             this.lblProductoMasVendido.Name = "lblProductoMasVendido";
-            this.lblProductoMasVendido.Size = new System.Drawing.Size(404, 30);
+            this.lblProductoMasVendido.Size = new System.Drawing.Size(487, 30);
             this.lblProductoMasVendido.TabIndex = 22;
             this.lblProductoMasVendido.Text = "hhh";
             this.lblProductoMasVendido.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // radioButtonClientes
+            // 
+            this.radioButtonClientes.AutoSize = true;
+            this.radioButtonClientes.Location = new System.Drawing.Point(32, 94);
+            this.radioButtonClientes.Name = "radioButtonClientes";
+            this.radioButtonClientes.Size = new System.Drawing.Size(179, 17);
+            this.radioButtonClientes.TabIndex = 24;
+            this.radioButtonClientes.TabStop = true;
+            this.radioButtonClientes.Text = "TOP3 Clientes con mas compras";
+            this.radioButtonClientes.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonCantProd
+            // 
+            this.radioButtonCantProd.AutoSize = true;
+            this.radioButtonCantProd.Location = new System.Drawing.Point(244, 94);
+            this.radioButtonCantProd.Name = "radioButtonCantProd";
+            this.radioButtonCantProd.Size = new System.Drawing.Size(189, 17);
+            this.radioButtonCantProd.TabIndex = 25;
+            this.radioButtonCantProd.TabStop = true;
+            this.radioButtonCantProd.Text = "Cantidad Productos por Categorias";
+            this.radioButtonCantProd.UseVisualStyleBackColor = true;
+            // 
+            // lblCliente1
+            // 
+            this.lblCliente1.BackColor = System.Drawing.Color.White;
+            this.lblCliente1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCliente1.Location = new System.Drawing.Point(29, 524);
+            this.lblCliente1.Name = "lblCliente1";
+            this.lblCliente1.Size = new System.Drawing.Size(477, 30);
+            this.lblCliente1.TabIndex = 26;
+            this.lblCliente1.Text = "hhh";
+            this.lblCliente1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Graficos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1184, 561);
+            this.Controls.Add(this.lblCliente1);
+            this.Controls.Add(this.radioButtonCantProd);
+            this.Controls.Add(this.radioButtonClientes);
             this.Controls.Add(this.lblProductoMasVendido);
             this.Controls.Add(this.chartProdTop5);
             this.Controls.Add(this.chartProdPorCat);
@@ -103,6 +143,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.chartProdPorCat)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartProdTop5)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -111,5 +152,8 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chartProdPorCat;
         private System.Windows.Forms.DataVisualization.Charting.Chart chartProdTop5;
         private System.Windows.Forms.Label lblProductoMasVendido;
+        private System.Windows.Forms.RadioButton radioButtonClientes;
+        private System.Windows.Forms.RadioButton radioButtonCantProd;
+        private System.Windows.Forms.Label lblCliente1;
     }
 }
--- ElectroMarket/CapaPresentacion/FormAdmin/Graficos.cs
+++ ElectroMarket/CapaPresentacion/FormAdmin/Graficos.cs
@@ -32,30 +32,73 @@ namespace CapaPresentacion.FormAdmin
 
         private void Graficos_Load(object sender, EventArgs e)
         {
-            GrafCate();
-            prodTop5ven();
+            //  cantProdPorCat();
+             prodTop5ven();
+            //clientesMasCompras();
+
+            // Configurar el evento CheckedChanged para los RadioButtons
+            radioButtonCantProd.CheckedChanged += (s, ev) => MostrarGraficoSeleccionado();
+            radioButtonClientes.CheckedChanged += (s, ev) => MostrarGraficoSeleccionado();
+
+            // Seleccionar uno de los RadioButtons por defecto
+            radioButtonCantProd.Checked = true;
+
         }
-        ArrayList categoria = new ArrayList();
-        ArrayList cantProd = new ArrayList();
+
 
         ArrayList Producto = new ArrayList();
         ArrayList Cant = new ArrayList();
 
 
-        private void GrafCate()
+        private void cantProdPorCat()
         {
+            // Declara e inicializa las listas dentro de la función
+            ArrayList categoria = new ArrayList();
+            ArrayList cantProd = new ArrayList();
+
             cmd = new SqlCommand("prodPorCateg", oConexion);
             cmd.CommandType = CommandType.StoredProcedure;
             oConexion.Open();
             dr = cmd.ExecuteReader();
+
+            string categoriaConMasProductos = "";
+            int cantidadMaxima = 0;
+
             while (dr.Read())
             {
-                categoria.Add(dr.GetString(0));
-                cantProd.Add(dr.GetInt32(1).ToString());
+                string categoriaActual = dr.GetString(0);
+                int cantidadActual = dr.GetInt32(1);
+
+                // Agregar datos al gráfico
+                categoria.Add(categoriaActual);
+                cantProd.Add(cantidadActual.ToString());
+
+                // Verificar si la cantidad actual es mayor que la máxima
+                if (cantidadActual > cantidadMaxima)
+                {
+                    cantidadMaxima = cantidadActual;
+                    categoriaConMasProductos = categoriaActual;
+                }
             }
+
             chartProdPorCat.Series[0].Points.DataBindXY(categoria, cantProd);
             dr.Close();
             oConexion.Close();
+
+            // Mostrar la categoría con más productos en el Label
+            MostrarCategoriaConMasProductos(categoriaConMasProductos, cantidadMaxima);
+        }
+
+        private void MostrarCategoriaConMasProductos(string categoriaConMasProductos, int cantidadMaxima)
+        {
+            if (!string.IsNullOrEmpty(categoriaConMasProductos))
+            {
+                lblCliente1.Text = $"Categoría con Más Productos: {categoriaConMasProductos} ({cantidadMaxima} productos)";
+            }
+            else
+            {
+                lblCliente1.Text = "No hay datos disponibles";
+            }
         }
 
         private void prodTop5ven()
@@ -87,6 +130,78 @@ namespace CapaPresentacion.FormAdmin
             }
         }
 
+        private void clientesMasCompras()
+        {
+            // Declara e inicializa las listas dentro de la función
+            ArrayList cliente = new ArrayList();
+            ArrayList cantCompras = new ArrayList();
 
+            cmd = new SqlCommand("SP_Top3ClientesMasCompras", oConexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            oConexion.Open();
+            dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                cliente.Add(dr.GetString(0));
+                cantCompras.Add(dr.GetInt32(1).ToString());
+            }
+
+            chartProdPorCat.Series[0].Points.DataBindXY(cliente, cantCompras);
+            dr.Close();
+            oConexion.Close();
+
+            // Después de obtener los datos y mostrarlos en el gráfico, obtener el cliente con más compras y mostrarlo en el Label
+            MostrarClienteConMasCompras(cliente, cantCompras);
+        }
+
+
+        private void MostrarClienteConMasCompras(ArrayList clientes, ArrayList cantidades)
+        {
+            // Verificar si hay datos en las listas
+            if (clientes.Count > 0 && cantidades.Count > 0)
+            {
+                // Encontrar el cliente con más compras
+                int indexClienteMasCompras = 0;
+                for (int i = 1; i < cantidades.Count; i++)
+                {
+                    int cantidadActual = Convert.ToInt32(cantidades[i]);
+                    int cantidadMaxima = Convert.ToInt32(cantidades[indexClienteMasCompras]);
+
+                    if (cantidadActual > cantidadMaxima)
+                    {
+                        indexClienteMasCompras = i;
+                    }
+                }
+
+                // Obtener el nombre del cliente con más compras y la cantidad
+                string clienteMasCompras = clientes[indexClienteMasCompras].ToString();
+                int cantidadCompras = Convert.ToInt32(cantidades[indexClienteMasCompras]);
+
+                // Mostrar la información en el Label
+                lblCliente1.Text = $"Cliente con Más Compras: {clienteMasCompras} ({cantidadCompras} compras)";
+            }
+            else
+            {
+                // Si no hay datos, mostrar un mensaje en el Label
+                lblCliente1.Text = "No hay datos disponibles";
+            }
+        }
+
+
+        private void MostrarGraficoSeleccionado()
+        {
+            // Limpiar datos existentes en los Charts
+            chartProdPorCat.Series[0].Points.Clear();
+
+            if (radioButtonCantProd.Checked)
+            {
+                cantProdPorCat(); // Muestra el gráfico de cantidad de productos por categoría
+            }
+            else if (radioButtonClientes.Checked)
+            {
+                clientesMasCompras(); // Muestra el gráfico de clientes con más compras
+            }
+        }
     }
 }
